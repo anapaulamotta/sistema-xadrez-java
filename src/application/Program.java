79be9893bdfcc93edfa3380ff8c6837909e88e61
Ana Motta <mottaanapaula98@gmail.com>
2022-03-09 13:17:54 -0300

    Primeira clase: Position

---
+++ src/application/Program.java
@@ -0,0 +1,15 @@
+package application;
+
+import boardGame.Position;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		Position position = new Position(8,8);
+		
+		System.out.println(position);
+		
+	}
+
+}
